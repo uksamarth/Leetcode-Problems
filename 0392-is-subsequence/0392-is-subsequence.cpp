6c53ea40f1664f43cda900f72fb84a8d7e35f295
U K Samarth <01fe21bec126@kletech.ac.in>
2024-08-11 17:49:58 +0530

    Time: 0 ms (100%), Space: 7.6 MB (67.38%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0,j=0;
+        while(i<s.length() && j<t.length()){
+            if(s[i] == t[j]){
+                i++;
+            }
+            j++;
+        }
+        if(i==s.length()){
+            return true;
+        }
+        return false;
+    }
+};
