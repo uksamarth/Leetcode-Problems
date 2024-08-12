3be7e263be2726c51100a23a63b4b2c00f590309
U K Samarth <01fe21bec126@kletech.ac.in>
2024-08-12 11:03:18 +0530

    Time: 7 ms (84.44%), Space: 10.9 MB (43.7%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,10 +11,12 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode* slow = head;
         ListNode* fast = head;
-        while(fast!=nullptr && fast->next!=nullptr){
+        while(fast!=nullptr && fast->next !=nullptr){
             slow = slow->next;
             fast = fast->next->next;
-            if(slow==fast) return true;
+            if(slow == fast){
+                return true;
+            }
         }
         return false;
     }
