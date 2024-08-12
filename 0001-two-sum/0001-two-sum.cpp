01bf4a365e9e58d87cdbe7f0e9f2c012231d391b
U K Samarth <01fe21bec126@kletech.ac.in>
2024-08-12 18:02:46 +0530

    Time: 6 ms (88.54%), Space: 14.7 MB (11.9%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    return {i,j};
-                }
+        map<int,int> res;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            int complement = target-nums[i];
+            if(res.count(complement)){
+                return {res[complement],i};
             }
+            res[nums[i]] = i;
         }
-        return {-1,-1};
+        return {};
+
     }
 };
