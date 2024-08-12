2db963602d20bcd7d63b2ce9c6333c5adbac760d
U K Samarth <01fe21bec126@kletech.ac.in>
2024-08-12 16:13:11 +0530

    Time: 182 ms (34.05%), Space: 122.9 MB (43.52%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,21 +10,59 @@
  */
 class Solution {
 public:
+    ListNode* reverseLinkedList(ListNode* head) {
+    // Check if the list is empty
+    // or has only one node
+    if (head == NULL || head->next == NULL) {
+        
+        // No change is needed;
+        // return the current head
+        return head; 
+    }
+
+    // Recursive step: Reverse the remaining 
+    // part of the list and get the new head
+    ListNode* newHead = reverseLinkedList(head->next);
+
+    // Store the next node in 'front'
+    // to reverse the link
+    ListNode* front = head->next;
+
+    // Update the 'next' pointer of 'front' to
+    // point to the current head, effectively
+    // reversing the link direction
+    front->next = head;
+
+    // Set the 'next' pointer of the
+    // current head to 'null' to
+    // break the original link
+    head->next = NULL;
+
+    // Return the new head obtained
+    // from the recursion
+    return newHead;
+}
     bool isPalindrome(ListNode* head) {
-        stack <int> st;
-        ListNode *temp = head;
-        while(temp!=nullptr){
-            st.push(temp->val);
-            temp = temp->next;
+        if(head == nullptr || head->next==nullptr){
+            return true;
         }
-        temp = head;
-        while(temp!=nullptr){
-            if(temp->val!=st.top()){
+        ListNode *slow = head, *fast = head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode *newhead= reverseLinkedList(slow);
+        ListNode *first = head, *second = newhead;
+        while(second!=nullptr){
+            if(first->val!=second->val){
+                reverseLinkedList(newhead);
                 return false;
             }
-            st.pop();
-            temp = temp->next;
+            first = first->next;
+            second = second->next;
         }
+        reverseLinkedList(newhead);
         return true;
+
     }
 };
