fae981945897841e752ef6d7ff0f5c42dd09a861
U K Samarth <01fe21bec126@kletech.ac.in>
2024-08-15 19:01:20 +0530

    Time: 90 ms (65.48%), Space: 61.1 MB (84.16%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int> mpp;
-        for(int i = 0;i<nums.size();i++){
-            if(mpp[nums[i]]!=0){
+        sort(nums.begin(),nums.end());
+        for(int i = 1;i< nums.size();i++){
+            if(nums[i]==nums[i-1]){
                 return true;
             }
-            mpp[nums[i]]++;
         }
         return false;
-
     }
 };
