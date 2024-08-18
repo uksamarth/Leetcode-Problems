f2e14c0283effb36630ccc5f5feec2053a76a28f
U K Samarth <01fe21bec126@kletech.ac.in>
2024-08-18 11:15:45 +0530

    Time: 0 ms (100%), Space: 7.9 MB (55.93%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    string mergeAlternately(string word1, string word2) {
-        int n = word1.length() + word2.length();
-        string res(n, ' ');  // Initialize a string with n spaces
-        int i = 0, j = 0, k = 0;
-        
-        // Merge characters alternately
-        while (i < word1.length() && j < word2.length()) {
-            if (k % 2 == 0) {
-                res[k++] = word1[i++];
-            } else {
-                res[k++] = word2[j++];
+    string mergeAlternately(string str1, string str2) {
+        int i=0,j=0,k=0;
+        int n = str1.length()+str2.length();
+        string res(n, ' ');
+        while(i<str1.length() && j<str2.length()){
+            if(k%2==0){
+                res[k++] = str1[i++];
+            }
+            else{
+                res[k++] = str2[j++];
             }
         }
-        
-        // Append the remaining characters from word1
-        while (i < word1.length()) {
-            res[k++] = word1[i++];
+        while(i<str1.length()){
+            res[k++] = str1[i++];
         }
-        
-        // Append the remaining characters from word2
-        while (j < word2.length()) {
-            res[k++] = word2[j++];
+        while(j<str2.length()){
+            res[k++] = str2[j++];
         }
-        
         return res;
     }
 };
