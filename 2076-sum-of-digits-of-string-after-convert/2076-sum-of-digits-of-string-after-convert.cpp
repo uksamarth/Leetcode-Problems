533087b1bb98781a11a00bf1ba3cf905867f9b17
U K Samarth <01fe21bec126@kletech.ac.in>
2024-09-03 19:53:16 +0530

    Time: 0 ms (100%), Space: 7.9 MB (77.66%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        int res = 0;
+        for(char c: s){
+            int num = c - 'a'+1;
+            while(num>0){
+                int last = num%10;
+                res +=last;
+                num/=10;
+            }
+        }
+        while(--k){
+            int new_res=0;
+            while(res>0){
+                new_res +=res%10;
+                res/=10;
+            }
+            res = new_res;
+        }
+        return res;
+    }
+};
