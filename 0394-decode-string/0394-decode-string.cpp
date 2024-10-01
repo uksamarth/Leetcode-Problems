e0d6befe3ab243a68ccffe3b1c30bc0646b0c540
U K Samarth <01fe21bec126@kletech.ac.in>
2024-10-01 19:41:21 +0530

    Time: 3 ms (41%), Space: 8.1 MB (67.45%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    // 3[a]2[bc] 
+    string helper(int &idx, string str){
+        int n=0;
+        string ans="";
+
+        while(idx<str.size()){
+            char curr=str[idx];
+            if(curr>='0' && curr <='9'){
+                n= n*10 + curr- '0';
+            }
+            else if( curr=='['){
+                string temp=helper(++idx, str);
+                // while(str[idx]!=']'){
+                //     temp+= str[idx];
+                //     idx++;
+                // }
+                while(n>0){
+                    ans+= temp;
+                    n--;
+                }
+            }
+            else if(curr==']'){
+                return ans;
+            }
+            else{
+                ans+=curr;
+            }
+            idx++;
+        }
+        return ans;
+        
+    }
+    string decodeString(string s) {
+        int i=0;
+        return helper(i,s);
+    }
+};
